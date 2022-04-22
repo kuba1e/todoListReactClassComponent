e0cee3d29bc66918d6f0a0c39091a97da132b02b
Yakov P <yakov.petryk@gmail.com>
2022-04-22 13:19:03 +0300

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.scss";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Hello mutherfucker, </h1>
+    </div>
+  );
+};
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: ["@babel/polyfill", "./src/index.jsx"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js",
+  },
+  devServer: {
+
+    watchFiles: ["./src/*.html"],
+    hot: true,
+    open: true,
+    port: 3000,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({ template: "./src/index.html" }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          {loader:"style-loader"},
+          // Translates CSS into CommonJS
+          {loader:"css-loader"},
+          // Compiles Sass to CSS
+          {loader:"sass-loader"},
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images",
+              name: "[name]-[hash].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.m?jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
