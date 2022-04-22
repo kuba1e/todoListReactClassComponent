bd3417716ff670ca81b7e72e19fca7e2aff4bd9b
Yakov P <yakov.petryk@gmail.com>
2022-04-22 16:29:31 +0300

    added style handing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
+import img from "../../../public/images/photo.jpg";
 
 export const App = () => {
   return (
     <div>
-      <h1>Hello mutherfucker, </h1>
+      <h1>Hello mutherfucker</h1>
+      <input id="name" />
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,116 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: ["@babel/polyfill", "./src/index.jsx"],
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[hash].js",
-  },
-  devServer: {
+module.exports = (env = {}) => {
+  const mode = env.production ? "production" : "development";
 
-    watchFiles: ["./src/*.html"],
-    hot: true,
-    open: true,
-    port: 3000,
-  },
-  plugins: [
-    new HTMLWebpackPlugin({ template: "./src/index.html" }),
-    new CleanWebpackPlugin(),
-  ],
-  module: {
-    rules: [
+  const getStyleLoaders = () => {
+    return [
+      mode === "production"
+        ? { loader: MiniCssExtractPlugin.loader }
+        : { loader: "style-loader" },
       {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          {loader:"style-loader"},
-          // Translates CSS into CommonJS
-          {loader:"css-loader"},
-          // Compiles Sass to CSS
-          {loader:"sass-loader"},
-        ],
+        loader: "css-loader",
       },
-      {
-        test: /\.(jpg|jpeg|png|svg)/i,
-        use: [
-          {
-            loader: "file-loader",
+    ];
+  };
+
+  const getPlugins = () => {
+    const plugins = [
+      new HTMLWebpackPlugin({
+        title: "Webpack test",
+        template: "./public/index.html",
+      }),
+      new CleanWebpackPlugin(),
+    ];
+
+    if (mode === "production") {
+      plugins.push(
+        new MiniCssExtractPlugin({
+          filename: "[name].[hash].min.css",
+        })
+      );
+    }
+
+    return plugins;
+  };
+
+  return {
+    mode,
+    entry: ["@babel/polyfill", "./src/index.jsx"],
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: mode === "production" ? "[name].[hash].min.js" : undefined,
+    },
+    devServer: {
+      watchFiles: ["./public/*.html"],
+      hot: true,
+      open: true,
+      port: 3000,
+    },
+    plugins: getPlugins(),
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
             options: {
-              outputPath: "images",
-              name: "[name]-[hash].[ext]",
+              presets: ["@babel/preset-env"],
             },
           },
-        ],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
+        },
+        {
+          test: /\.m?jsx$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-react", "@babel/preset-env"],
+            },
           },
         },
-      },
-      {
-        test: /\.m?jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react", "@babel/preset-env"],
-          },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            ...getStyleLoaders(),
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [["autoprefixer"]],
+                },
+              },
+            },
+            { loader: "sass-loader" },
+          ],
         },
-      },
-    ],
-  },
+        //loading images
+        {
+          test: /\.(jpg|jpeg|png|svg)$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "images",
+                name(resourcePath, resourceQuery) {
+                  // `resourcePath` - `/absolute/path/to/file.js`
+                  // `resourceQuery` - `?foo=bar`
+
+                  if (mode === "development") {
+                    return "[name].[ext]";
+                  }
+
+                  return "[contenthash].[ext]";
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+  };
 };
