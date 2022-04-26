121c57ca3fe2e3b4d5973375dfb2cae37de1f470
Yakov P <yakov.petryk@gmail.com>
2022-04-26 14:20:55 +0300

    <feat> add responsive styles

--- src/components/ErrorIndicator/ErrorIndicator.jsx
+++ src/components/ErrorIndicator/ErrorIndicator.jsx
@@ -5,7 +5,11 @@ export default class ErrorIndicator extends Component {
   render() {
     const { error } = this.props
 
-    return <p>Ooops, something went wrong, error:{error}</p>
+    return (
+      <div className='error-boundray'>
+        <p>Ooops, something went wrong, error:{error}</p>
+      </div>
+    )
   }
 }
 
