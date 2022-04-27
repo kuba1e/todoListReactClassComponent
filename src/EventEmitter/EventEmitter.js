9a8b50323d019aa91dc427f06bda4c8e7ff0933e
Yakov P <yakov.petryk@gmail.com>
2022-04-27 23:16:25 +0300

    refactor: add private class properties

--- src/EventEmitter/EventEmitter.js
+++ src/EventEmitter/EventEmitter.js
@@ -1,24 +1,22 @@
 class EventEmitter {
-  constructor() {
-    this.events = {}
-  }
+  #events = {}
 
   subscribe = (name, listenerFunc) => {
-    if (!this.events[name]) {
-      this.events[name] = []
+    if (!this.#events[name]) {
+      this.#events[name] = []
     }
-    this.events[name].push(listenerFunc)
+    this.#events[name].push(listenerFunc)
   }
 
   deleteSubscribe = (name, listenerFunc) => {
-    this.events[name] = this.events[name].filter(
+    this.#events[name] = this.#events[name].filter(
       (listener) => listener !== listenerFunc
     )
   }
 
   emit = (name, ...data) => {
     const listener = (callback) => callback(...data)
-    this.events[name].forEach(listener)
+    this.#events[name].forEach(listener)
   }
 }
 
