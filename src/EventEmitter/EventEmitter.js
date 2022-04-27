fa6606ebea8430a48781c7c8a66f19e6f0256165
Yakov P <yakov.petryk@gmail.com>
2022-04-27 11:02:52 +0300

    feat: add event emitter

--- src/EventEmitter/EventEmitter.js
+++ src/EventEmitter/EventEmitter.js
@@ -1,13 +1,45 @@
-import EventEmitter from 'events'
+class EventEmitter {
+  constructor() {
+    this.events = {}
+  }
 
-const EventEmitterInst = new EventEmitter()
+  static addSubscribe = (name) => {
+    this.events[name] = []
+  }
 
+  subscribe = (name, listenerFunc) => {
+    if (!this.events[name]) {
+      EventEmitter.addSubscribe(name)
+    }
+    this.events[name].push(listenerFunc)
+    return () => {
+      this.events[name] = this.events[name].filter(
+        (listener) => listener !== listenerFunc
+      )
+    }
+  }
+
+  deleteSubscribe = (name, listenerFunc) => {
+    this.events[name] = this.events[name].filter(
+      (listener) => listener !== listenerFunc
+    )
+  }
+
+  emit = (name, data) => {
+    const listener = (callback) => [callback(data)]
+    this.events[name].forEach(listener)
+  }
+}
+
+const emitter = new EventEmitter()
+/*
 const emitter = {
   on: (event, fn) => EventEmitterInst.on(event, fn),
   off: (event, fn) => EventEmitterInst.off(event, fn),
   emit: (event, payload) => EventEmitterInst.emit(event, payload)
 }
+*/
 
-Object.freeze(emitter)
+// Object.freeze(emitter)
 
 export default emitter
